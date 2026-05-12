71e9e4f6d6508b1f2bff6b8f05bb805ccce4c8fa
marbin701 <marvin07012002@gmail.com>
2026-05-12 16:27:49 -0400

    getter and setter in the vehiclle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -10,6 +10,22 @@ public class Vehicle extends Asset {
         this.odometer = odometer;
     }
 
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     public int getOdometer() {
         return odometer;
     }
