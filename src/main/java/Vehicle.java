a36ba0ca8158386103eb630cc713bd2260f54327
marbin701 <marvin07012002@gmail.com>
2026-05-12 16:26:37 -0400

    Asset class, House class and Vehicle class created. Also already had work on Asst class and house class. and is currently working in vehicle class

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,20 @@
+public class Vehicle extends Asset {
+    private String makeModel;
+    private int year;
+    private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+}
