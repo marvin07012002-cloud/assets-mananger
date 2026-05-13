002e206ad73d6fb2f05d6c22293316784a4c86c9
marbin701 <marvin07012002@gmail.com>
2026-05-13 07:52:10 -0400

    Houses in the main class added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,17 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Asset> assetArrayList;
-        assetArrayList =
+        ArrayList<Asset> assetArrayList = new ArrayList<>();
+
+        House europeHouse = new House("Mansion in Switzerland", "08/26/2028", 3045908.45,
+                "random street 99", 10, 10000, 10000);
+
+        House americaHouse = new House("Penthouse in New york", "09/26/2028", 4045908.95,
+                "Madison square", 34, 4200, 234000);
+
+
+
+
+
     }
 }
